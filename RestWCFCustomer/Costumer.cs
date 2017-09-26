ed80e9ddfe7461a4a1793408f73420ea6785b797
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-26 12:50:32 +0200

    Tilføjet properties i klassen cosutmer. Dertil using runtime.serialization.

--- RestWCFCustomer/Costumer.cs
+++ RestWCFCustomer/Costumer.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace RestWCFCustomer
 {
+    [DataContract]
     public class Costumer
     {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public int MyProperty { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
     }
 }
