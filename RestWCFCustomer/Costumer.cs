5594a8e1db2568bbb33c8963e9ad4dcd0929081b
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-26 12:54:40 +0200

    Implemeneteret konstruktøre i klassen customer. En default og en overloaded med 4 lokale parametre.

--- RestWCFCustomer/Costumer.cs
+++ RestWCFCustomer/Costumer.cs
@@ -6,23 +6,54 @@ using System.Web;
 
 namespace RestWCFCustomer
 {
+    /// <summary>
+    /// Denne klasse indeholder attributter for costumer.
+    /// </summary>
     [DataContract]
     public class Costumer
     {
+        /// <summary>
+        /// Prop Id
+        /// </summary>
         [DataMember]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Prop FirstName
+        /// </summary>
         [DataMember]
         public string FirstName { get; set; }
+        /// <summary>
+        /// Prop LastName
+        /// </summary>
 
         [DataMember]
         public string LastName { get; set; }
 
-        [DataMember]
-        public int MyProperty { get; set; }
-
+        /// <summary>
+        /// prop year
+        /// </summary>
         [DataMember]
         public int Year { get; set; }
+
+        /// <summary>
+        /// Default konstruktor - REST afhænger af en default, måske???
+        /// </summary>
+        public Costumer()
+        {
+
+        }
+        /// <summary>
+        /// Overloaded konstruktør som initierer en instans costumer med 4 atributter. 
+        /// </summary>
+        public Costumer(int id, string firstname, string lastname, int year)
+        {
+            this.Id = id;
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Year = year;
+        }
+
 
     }
 }
